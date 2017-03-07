c8d873edaa300e3ffc4319adde7dcc9094a5d9c4
HDB <TBC3211@GMAIL.COM>
2017-03-06 17:33:40 -0800

    Update probB.R

--- probB.R
+++ probB.R
@@ -1,5 +1,7 @@
+
+
 nbytes <- function(drname,filelist,arg){
-  
+  dirname = drname
   for (f in filelist){
     arg = arg + file.info(f)$size
   }
@@ -8,23 +10,31 @@ nbytes <- function(drname,filelist,arg){
   
 }
 
-rmemptydirs <- function(drname,filelist,arg){
+rmemptydirs <- function(drname,filelist){
+  for (f in 1:length(filelist)){
+    folder_size=sum(file.info(list.files(filelist[f], all.files = TRUE, recursive = TRUE))$size)
+     if (folder_size == 0 ){
+        unlink(filelist[f],recursive = T)
+     }
+  }
   
-  empty_flst <- ff[file.info(filelist)[["size"]]==0]
-  unlink(empty_flst)
 }
 
-walk <- function(currdir,f,arg){
-  droot <- currdir
-  dirlist <- list.dirs(droot,full.names=TRUE,recursive = TRUE)
-  for (i in dirlist)
-  {
-    setwd(i)
-    sub_flst <- list.files(path = ".",full.names=TRUE)
-    f(sub_flst,arg)
+walk <- function(currdir,f,first){
+  if (first ==T) {
+    droot <- currdir
   }
+  dirlist <- dir(currdir,full.names = T)[file.info(dir(currdir,all.files = FALSE,full.names=T))$isdir]
+  f(currdir,dirlist)
+  dirlist <- dir(currdir,full.names = T)[file.info(dir(currdir,all.files = FALSE,full.names=T))$isdir]
+  for (i in 1:length(dirlist))
+  {
+    walk(dirlist[i],f,FALSE)
+    
+  }
+  
   setwd(droot)
 }
 
-
-
+walk(getwd(),rmemptydirs,first = T)
+getwd()
